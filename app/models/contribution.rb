b59b3203f8a4bc43a84325e2632f0cfa232cb46b
Emman <emmanuel.goh.7@gmail.com>
2016-09-12 17:20:58 +0800

    Now validate the suggestor and the contributor

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -3,7 +3,7 @@
 class Contribution < ActiveRecord::Base
   belongs_to :gift
 
-  validates :gift, :amount, presence: true
+  validates :gift, :amount, :creator, presence: true
   validates :amount, numericality: { greater_than: 0 }
 
   stampable
