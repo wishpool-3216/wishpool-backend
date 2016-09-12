36237b1f776ee89d0740806dd72ca5ca86f41291
Emman <emmanuel.goh.7@gmail.com>
2016-09-12 17:42:06 +0800

    Validate publicity status for gifts

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,7 +1,18 @@
 class Gift < ActiveRecord::Base
   belongs_to :recipient, class_name: 'User'
 
-  validates :recipient, :name, :creator, presence: true
+  validates :recipient, :name, :creator,
+            :publicity, presence: true
+
+  ##
+  # The publicity status of a gift
+  # Public -> Recipient can see
+  # Private -> Recipient will be unaware
+  # Note: Started from 1 instead of 0 cos we lazy with validations
+  enum publicity: {
+    'Public' => 1,
+    'Private' => 2
+  }
 
   stampable
 end
