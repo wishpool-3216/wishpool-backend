826123a76d63aabbf114ef63bd44eb0a7b6cf41b
Emman <emmanuel.goh.7@gmail.com>
2016-09-13 19:16:57 +0800

    Rewrite auth API

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/auth/:provider/callback', to: 'sessions#create'
+  post '/auth/:provider/callback', to: 'sessions#create'
 
   mount_devise_token_auth_for 'User', at: 'auth'
   # The priority is based upon order of creation: first created -> highest priority.
