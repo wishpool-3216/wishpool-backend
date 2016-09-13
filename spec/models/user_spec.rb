767b51cf6906eed2f343d1f6df377a7bedeb0d8b
Emman <emmanuel.goh.7@gmail.com>
2016-09-13 19:17:05 +0800

    Fix tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,25 @@
 require 'rails_helper'
 
 describe User do
-  AUTH_HASH = {
-    'provider' => 'facebook',
-    'uid' => '12345',
-    'info' => {
-      'name' => 'Test User'
-    },
-    'credentials' => {
-      'token' => '16938',
-      'expires_at' => Time.new
-    }
-  }.freeze
+  AUTH = [
+    'facebook',
+    '12345',
+    '16931248',
+    Time.new
+  ].freeze
 
   it 'should create a new user if one does not exist' do
     expect do
-      User.find_or_create_from_auth_hash(AUTH_HASH)
+      User.find_or_create_from_auth_hash(*AUTH)
     end.to change { User.count }.by(1)
   end
 
   it 'should update the token of an existing user' do
-    User.find_or_create_from_auth_hash(AUTH_HASH)
+    User.find_or_create_from_auth_hash(*AUTH)
     expect do
-      auth = AUTH_HASH.dup
-      auth['credentials']['token'] = 'new_token'
-      @user = User.find_or_create_from_auth_hash(auth)
+      auth = AUTH.dup
+      auth[2] = 'new_token'
+      @user = User.find_or_create_from_auth_hash(*auth)
     end.to_not change { User.count }
     expect(@user.oauth_token).to eq('new_token')
   end
